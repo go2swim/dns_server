d7d64775544e9f96dfdc56a83b42b528accabbec
Andrey <Andreyvol9898@gmail.com>
2024-11-06 12:58:03 +0500

    end

--- tests.py
+++ tests.py
@@ -6,13 +6,12 @@ class TestExtractIP(unittest.TestCase):
     def test1(self):
         resolver = DNSResolver()
         ips = resolver.resolve("urfu.ru")
-        self.assertEqual(ips, "0.0.0.0")
+        self.assertEqual(ips, ['93.88.179.201'])
 
-    #
-
-    #
-
-    #
+    def test2(self):
+        resolver = DNSResolver()
+        ips = resolver.resolve("ya.ru")
+        self.assertEqual(ips, ['77.88.44.242', '5.255.255.242', '77.88.55.242'])
 
 
 if __name__ == "__main__":
